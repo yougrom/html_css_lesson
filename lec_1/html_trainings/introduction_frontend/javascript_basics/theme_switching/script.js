205e305338e723e5aef2e437a0a000a67618f5a2
herrGromov <youriy.gromov@gmail.com>
2023-07-10 18:37:42 +0200

    finisch width switcher and add to mail button event

--- lec_1/html_trainings/introduction_frontend/javascript_basics/theme_switching/script.js
+++ lec_1/html_trainings/introduction_frontend/javascript_basics/theme_switching/script.js
@@ -1,7 +1,16 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
-
 themeButton.onclick = function() {
   page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme');
+};
+
+let message = document.querySelector('.subscription-message');
+let form = document.querySelector('.subscription');
+let email = document.querySelector('.subscription-email');
+
+form.onsubmit = function(evt) {
+  evt.preventDefault();
+  // Измените значение textContent на следующей строке
+  message.textContent = 'Адрес ' + email.value + ' добавлен в список получателей рассылки.';
 };
