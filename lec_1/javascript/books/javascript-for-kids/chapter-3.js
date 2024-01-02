1f5b1ab76a0544cb6aacf62e49637574624ed099
Herr Gromov <youriy.gromov@gmail.com>
2024-01-02 15:13:50 +0100

    The Arrays Example Exrcises

--- lec_1/javascript/books/javascript-for-kids/chapter-3.js
+++ lec_1/javascript/books/javascript-for-kids/chapter-3.js
@@ -91,6 +91,7 @@ let firstAnimal = animals.shift();
 console.log(animals); // ["Lama"]
 console.log(firstAnimal); // Cow
 
+
 // методы unshift и shift добавляют и удаляют элементы с начала массива.
 // методы push и pop добавляют и удаляют элементы с конца массива.
 
@@ -111,6 +112,10 @@ let colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
 console.log(colors.indexOf('blue')); // 2
 console.log(colors[2]); // blue
 
+// если элемента нет в массиве, то метод indexOf вернёт -1
+console.log(colors.indexOf('pink')); // -1
+
+
 // простой способ получения последнего элемента массива
 console.log(colors.length); // 6 - элементов массива всегда на одну позицию больше, чем длина массива
 // последний элемент массива всегда находится на одну позицию меньше, чем длина массива
@@ -122,5 +127,92 @@ console.log(colors.indexOf('purple')); // 5
 
 // поиск индекса элемента, которого нет в массиве
 console.log(colors.indexOf('pink')); // -1 - если элемента нет в массиве, то метод indexOf вернёт -1
+// если элемент в массиве встречается больше чем один раз, то метод indexOf вернёт индекс первого вхождения элемента в массив.
 
-// если элемент массиве встречается больше чем один раз, то метод indexOf вернёт индекс первого вхождения элемента в массиве метод индекс он вернёт индекс
+
+// превращаем массив в строку с помощью метода .join() - метод .join() возвращает строку которую записаны элементы массива, разделённые запятой
+let boringAnimals = ['Monkey', 'Cat', 'Fish', 'Lizard'];
+console.log(boringAnimals.join()); // Monkey,Cat,Fish,Lizard
+// можно указать разделитель в скобках если мы не хотим использовать запятую .join(' - ');
+console.log(boringAnimals.join(' - ')); // Monkey - Cat - Fish - Lizard
+console.log(boringAnimals.join(' ')); // Monkey Cat Fish Lizard
+
+// если в массиве хранятся не строковое значения, то метод Join преобразует их в строки
+let numbers = [11, 04, 1987];
+console.log(numbers.join(' ')); // 11 04 1987
+console.log(numbers.join('-')); // 11-04-1987
+
+
+
+// что полезного можно сделать с массивами
+// метод .push(); и .pop();
+let landmarks = [];
+landmarks.push('My location'); // добавляем элемент в конец массива с помощью метода .push();
+landmarks.push('My location 2');
+landmarks.push('My location 3');
+console.log(landmarks); // ["My location", "My location 2", "My location 3"]
+
+landmarks.pop(); // удаляем последний элемент массива с помощью метода .pop();  
+console.log(landmarks); // ["My location", "My location 2"]
+landmarks.pop();
+console.log(landmarks); // ["My location"]
+
+
+// Math.random() - возвращает случайное число от 0 до 1 (не включая 1)
+console.log(Math.random()); // 0.123456789
+console.log(Math.random()); // 0.987654321
+
+// Math.random() * 10 - возвращает случайное число от 0 до 10 (не включая 10)
+console.log(Math.random() * 10); // 0.123456789 * 10 = 1.23456789
+console.log(Math.random() * 10); // 0.987654321 * 10 = 9.87654321
+
+// Math.floor() - округляет число в меньшую сторону до целого числа (отбрасывает дробную часть)
+console.log(Math.floor(1.23456789)); // 1
+console.log(Math.floor(9.87654321)); // 9
+
+// Math.floor(Math.random() * 10) - возвращает случайное число от 0 до 9 (не включая 10)
+console.log(Math.floor(Math.random() * 4)); // 0, 1, 2, 3 
+
+let lenghtArr = Math.random();
+console.log(Math.floor(lenghtArr() * 7)); // 0, 1, 2, 3, 4, 5, 6
+
+// Example
+
+let randomWords = ['Планета', 'Звезда', 'Галактика', 'Космос']; 
+let randomIndeks = Math.floor(Math.random() * 4);
+console.log(randomWords[randomIndeks]); // Звезда, Галактика, Космос, Планета - случайное слово из массива
+// или  можно записать так
+console.log(randomWords[Math.floor(Math.random() * 4)]); // Звезда, Галактика, Космос, Планета - случайное слово из массива
+
+
+// Example
+let phrases = ['Звучит неплохо', 
+            'Да, это определённо надо сделать', 
+            'Не думаю, что это хорошая идея', 
+            'Может, не сегодня?', 
+            'Компьютер говорит нет'];
+console.log(phrases[Math.floor(Math.random() * 5)]); // Звучит неплохо, Да, это определённо надо сделать, Не думаю, что это хорошая идея, Может, не сегодня?, Компьютер говорит нет - случайное слово из массива
+
+
+// генератор случайных фраз
+let randomBodyParts = ['глаз', 'нос', 'череп'];
+let randomAdjectives = ['вонючая', 'унылая', 'дурацкая'];
+let randomNewWords = ['муха', 'выдра', 'дубина', 'мартышка', 'крыса'];
+
+let randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)]; 
+let randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+let randomNewWord = randomNewWords[Math.floor(Math.random() * 5)];
+// или можно записать так
+let randomBodyPartLenght = randomBodyParts[Math.floor(Math.random() * randomBodyParts.length)];
+let randomAdjectiveLenght = randomAdjectives[Math.floor(Math.random() * randomAdjectives.length)];
+let randomNewWordLenght = randomNewWords[Math.floor(Math.random() * randomNewWords.length)];
+
+let randomInsult = "У тебя " + randomBodyPart + " словно " + randomAdjective + " " + randomNewWord + " !!!";
+console.log(randomInsult); // У тебя череп словно унылая дубина!!!
+
+// или можно записать так
+
+let randomInsultLenght = ["У тебя", randomBodyPartLenght, "словно", randomAdjectiveLenght, randomNewWordLenght, "!!!"].join(" ");
+console.log(randomInsultLenght); 
+
+
